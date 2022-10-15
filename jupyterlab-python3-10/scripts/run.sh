585cb71405f8801f8a506949cc6b474e64da6c02
v-crn <var.cyrano@gmail.com>
2022-10-15 16:50:42 +0900

    add jupyterlab-python3-10

---
+++ jupyterlab-python3-10/scripts/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+first_time_flg=/tmp/first_time_flg
+if [ ! -e $first_time_flg ]; then
+    echo "First time setup"
+    touch $first_time_flg
+
+    sh scripts/jupyter/jupyter_init.sh
+
+    echo "......Done."
+fi
+
+jupyter lab --allow-root --ip 0.0.0.0 --port ${PORT}
